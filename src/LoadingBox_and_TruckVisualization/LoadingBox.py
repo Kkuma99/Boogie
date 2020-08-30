16c8a202c2848c875ac992d48fe8a9725a23f0e5
MiGyeong <46590577+kmk3942@users.noreply.github.com>
2020-08-30 18:35:09 +0900

    Update LoadingBox.py

--- src/LoadingBox_and_TruckVisualization/LoadingBox.py
+++ src/LoadingBox_and_TruckVisualization/LoadingBox.py
@@ -1,4 +1,5 @@
-# 길이 제한에 대한 코드 수정 필요
+# 이 코드의 random input으로 실행 가능
+
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product
@@ -12,7 +13,7 @@ import sys
 # Define
 ## Number of locals and boxes
 NUM_LOCAL = 3
-NUM_BOX = [30, 30, 30]  # 각 지역별 상자 개수
+NUM_BOX = [100, 100, 100]  # 각 지역별 상자 개수
 
 ## Locals
 LOCAL_A = 0
@@ -22,7 +23,7 @@ LOCAL_C = 2
 ## Size of truck(1cm unit)
 TRUCK_L = 50    # x
 TRUCK_W = 25    # y
-TRUCK_H = 30    # z
+TRUCK_H = 25    # z
 
 ## Boxes to load
 inputBox = {}
@@ -35,10 +36,10 @@ for i in range(0, NUM_LOCAL):
 ### Sample
 for i in range(0, NUM_LOCAL):
     for j in range(0, NUM_BOX[i]):
-        w = 5
-        l = 5
-        # w = random.randint(1, 7)
-        # l = random.randint(1, 7)
+        # w = 5
+        # l = 5
+        w = random.randint(1, 7)
+        l = random.randint(1, 7)
         inputBox[i][j]['w'] = w
         inputBox[i][j]['l'] = l
         inputBox[i][j]['h'] = BOX_H
@@ -56,29 +57,28 @@ print(check)
 ## Truck status
 truck = np.zeros((TRUCK_L, TRUCK_W, TRUCK_H), dtype=np.int8)
 
-##
-#endOfW = 0
-endOfL = 0  # 현재 층에서 가장 작은 길이를 측정하기 위한 변수
-#endOfH = 0
-
+## 측정을 위한 변수
 count_W = 0     # 상자를 적재할 빈 공간의 너비를 측정하기 위한 변수
-count_L = 0     # endOfL을 계산하기 위해 막힌 공간의 길이를 측정하기 위한 변수
-count_H = 0
-
-min_L = 0   # 적재된 차지한 가장 작은 길이
-# floor = 0   # 현재 적재하고 있는 층수
-boxIndex = 0    # 이번에 적재할 상자의 인덱스
-max_box_W = 0   # count_W 너비 안에 들어갈 수 있는 최대 너비의 상자 너비
-measureMode = 0     # 측정 모드 플래그: 비어있는 공간의 너비를 측정하고 있으면 1, 아니면 0
-sum_num_box = 0
+count_L = 0     # 막힌 공간의 길이를 측정하기 위한 변수
+count_H = 0     # 막힌 공간의 높이를 측정하기 위한 변수
 
 ## Position of box to load(원점과 가장 가까운 좌표)
 pos_X = 0
 pos_Y = 0
 pos_Z = 0
 
-## 각 지역별 적재 완료된 상자의 개수를 저장할 변수
-finish = [0, 0, 0]
+## 기타
+min_L = 0   # 적재된 상자들이 차지한 가장 작은 길이
+endOfL = 0  # 현재 층에서 가장 작은 길이를 측정하기 위한 변수
+floor = 0   # 현재 적재하고 있는 층수
+boxIndex = 0    # 이번에 적재할 상자의 인덱스
+max_box_W = 0   # count_W 너비 안에 들어갈 수 있는 최대 너비의 상자 너비
+measureMode = 0     # 측정 모드 플래그: 비어있는 공간의 너비를 측정하고 있으면 1, 아니면 0
+sum_num_box = 0  # 각 지역별 적재 범위를 계산하기 위함
+finish = [0, 0, 0]  # 각 지역별 적재 완료된 상자의 개수를 저장할 변수
+
+
+
 
 
 
@@ -115,11 +115,15 @@ for i in range(NUM_LOCAL):  # 각 지역별로 수행
             if count_L < endOfL:
                 endOfL = count_L
         print('endOfL: ', endOfL)
+        print('TRUCK_L * (sum_num_box / sum(NUM_BOX)): ', TRUCK_L * (sum_num_box / sum(NUM_BOX)))
 
         # 한 층에 각 지역에 할당된 길이만큼 적재되었거나 트럭 길이 끝까지 적재된 경우 층수 증가
         if endOfL >= TRUCK_L * (sum_num_box / sum(NUM_BOX)) or endOfL >= TRUCK_L:
             floor += 1
             print('increase the floor: ', floor)
+            if floor > TRUCK_H/BOX_H-1:
+                floor = 0
+                print("Truck Overflow!")
 
         # 상자를 적재할 위치 계산
         min_L = TRUCK_L     # 최소값을 찾기 위해 큰 값으로 초기화
@@ -152,57 +156,39 @@ for i in range(NUM_LOCAL):  # 각 지역별로 수행
             cannot_load = 0
             if check[i][j] == 0 and inputBox[i][j]['w'] <= count_W:     # 1. 아직 적재하지 않은 상자이고, 너비가 count_W 이하면
                 if inputBox[i][j]['w'] > max_box_W:     # 2. 최대 너비를 가진 상자를 찾음
-                    # 3. 적재할 수 있는지 확인
-                    for x in range(inputBox[i][j]['l']):
-                        for y in range(inputBox[i][j]['w']):
-                            # 4. 적재할 상자의 아래가 막혀있는지 확인
-                            if truck[pos_X+x][pos_Y+y][pos_Z-1] == 0:
-                                if pos_Z-1 != -1:   # 가장 아래층인 경우 Z좌표가 -1이므로 따로 조건을 줌
-                                    cannot_load = 1
-                                    break
-                        if cannot_load == 1:
-                            break
-                    if cannot_load == 1:
-                        continue
-                    # 5. 해당 위치에 상자를 적재했을 때 트럭 높이를 넘지 않는지 확인
+                    # 3. 해당 위치에 상자를 적재했을 때 트럭 높이를 넘지 않는지 확인
                     count_H = 0     # 해당 위치에 상자 적재 전 높이
                     while truck[pos_X][pos_Y][count_H] != 0:
                         count_H += 1
-                    if count_H+inputBox[i][j]['h'] <= TRUCK_H:
-                        boxIndex = j  # 상자 인덱스 저장
-                        max_box_W = inputBox[i][j]['w']  # 최대 너비 갱신
-                        # # 6. 해당 위치에 상자를 적재했을 때 트럭 길이를 넘지 않는지 확인
-                        # count_L = 0
-                        # while truck[count_L][pos_Y][pos_Z] != 0:
-                        #     count_L += 1
-                        # if count_L * inputBox[i][j]['l'] <= TRUCK_L:
-                        #     boxIndex = j  # 상자 인덱스 저장
-                        #     max_box_W = inputBox[i][j]['w']  # 최대 너비 갱신
-                        # else:
-                        #     continue
-                    else:
+                    if count_H+inputBox[i][j]['h'] > TRUCK_H:
                         continue
 
-        # # 상자 적재 또는 빈 공간 채우기
-        # if max_box_W == 0:  # 해당 공간에 적재할 수 있는 상자가 없다면 빈공간 채우기
-        #     # 2로 채움
-        #     truck[pos_X][pos_Y:pos_Y + count_W][pos_Z:pos_Z + BOX_H] = 2
-        # else:  # 해당 공간에 적재할 수 있는 상자가 있다면 상자 적재
-        #     l_len = inputBox[i][boxIndex]['l']
-        #     w_len = inputBox[i][boxIndex]['w']
-        #     h_len = inputBox[i][boxIndex]['h']
-        #     if i == 0:
-        #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 3
-        #     elif i == 1:
-        #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 4
-        #     else:
-        #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 5
+                    # 4. 해당 위치에 상자를 적재했을 때 트럭 길이를 넘지 않는지 확인
+                    count_L = 0
+                    while truck[count_L][pos_Y][pos_Z] != 0:
+                        count_L += 1
+                    if count_L+inputBox[i][j]['l'] > TRUCK_L:
+                        continue
 
+                    # 5. 적재할 상자의 아래가 막혀있는지 확인
+                    if pos_Z - 1 != -1:  # 가장 아래층인 경우 Z좌표가 -1이므로 따로 조건을 줌
+                        for x in range(inputBox[i][j]['l']):
+                            for y in range(inputBox[i][j]['w']):
+                                if truck[pos_X+x][pos_Y+y][pos_Z-1] == 0:
+                                    cannot_load = 1
+                                    break
+                        if cannot_load == 1:
+                            continue
+                    boxIndex = j  # 상자 인덱스 저장
+                    max_box_W = inputBox[i][j]['w']  # 최대 너비 갱신
+
+        # 상자 적재 또는 빈 공간 채우기
         if max_box_W == 0:  # 해당 공간에 적재할 수 있는 상자가 없다면 빈공간 채우기
             # 2로 채움
             for y in range(count_W):
                 for z in range(BOX_H):
                     truck[pos_X][pos_Y+y][pos_Z+z] = 2
+                    print("fill with 2")
         else:   # 해당 공간에 적재할 수 있는 상자가 있다면 상자 적재
             # 1로 채움
             for x in range(inputBox[i][boxIndex]['l']):
@@ -215,12 +201,28 @@ for i in range(NUM_LOCAL):  # 각 지역별로 수행
                         else:
                             truck[pos_X + x][pos_Y + y][pos_Z + z] = 5
                         # truck[pos_X + x][pos_Y + y][pos_Z + z] = 1
+
+            # if max_box_W == 0:  # 해당 공간에 적재할 수 있는 상자가 없다면 빈공간 채우기
+            #     # 2로 채움
+            #     truck[pos_X][pos_Y:pos_Y + count_W][pos_Z:pos_Z + BOX_H] = 2
+            # else:  # 해당 공간에 적재할 수 있는 상자가 있다면 상자 적재
+            #     l_len = inputBox[i][boxIndex]['l']
+            #     w_len = inputBox[i][boxIndex]['w']
+            #     h_len = inputBox[i][boxIndex]['h']
+            #     if i == 0:
+            #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 3
+            #     elif i == 1:
+            #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 4
+            #     else:
+            #         truck[pos_X:pos_X + l_len][pos_Y:pos_Y + w_len][pos_Z:pos_Z + h_len] = 5
+
             finish[i] += 1   # 적재 완료된 상자 수 갱신
             check[i][boxIndex] = 1  # 적재 완료된 상자 체크
             ##### display
+
         print('check: ', check)
         print('finish: ', finish)
-        print('-----------------------------')
+        print('------------------------------------------------------------------------')
 
 
 np.set_printoptions(threshold=sys.maxsize)
