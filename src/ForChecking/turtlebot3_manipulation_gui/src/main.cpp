33235140c00d9656b22aeed775426a3830b106e1
MinJi <46590583+Kkuma99@users.noreply.github.com>
2020-09-17 17:44:56 +0900

    Update main.cpp

--- src/ForChecking/turtlebot3_manipulation_gui/src/main.cpp
+++ src/ForChecking/turtlebot3_manipulation_gui/src/main.cpp
@@ -32,25 +32,25 @@ int main(int argc, char **argv) {
     QApplication app(argc, argv);
     turtlebot3_manipulation_gui::MainWindow w(argc,argv);
     char n[1];
-    std::cout << "주소지는 a/b/c/: ";
-
-    int a[4] = {};
-    int b[4] = {0.550, 0.638, 0.068, -0.005};
-    
-    std::cin >> n;
-    //w.show();
-    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
-    int result = app.exec();
-
+    w.show();
     w.on_btn_timer_start_clicked();
-    
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    int a[4] = {};
+    float b[4] = {0.550, 0.638, 0.068, -0.005};
+    std::cout << "주소지는 a/b/c/: ";
+    std::cin >> n;
+
     if(strcmp(n,"a")==0){
         // 행동 진행 
 	w.on_btn_init_pose_clicked();
-	    std::cout << "a\n";
+        //std::cin >> n;
+        //sleep(2000);
+        //w.on_btn_home_pose_clicked();
+	std::cout << "a\n";
     }
     else if(strcmp(n,"b")==0){
 	w.on_btn_send_joint_angle_clicked(b[0], b[1], b[2], b[3]);
+        std::cin >> n;
 	w.on_btn_init_pose_clicked();
 	//행동
 	std::cout << "b\n";
@@ -66,6 +66,6 @@ int main(int argc, char **argv) {
     //turtlebot3_manipulation_gui::MainWindow w(argc,argv);
     //w.show();
     //app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
-    //int result = app.exec();
+    int result = app.exec();
     return result;
 }
