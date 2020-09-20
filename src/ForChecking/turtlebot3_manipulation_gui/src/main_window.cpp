256189a088dc712223d8fa20056c746950e68852
MinJi <46590583+Kkuma99@users.noreply.github.com>
2020-09-20 15:24:08 +0900

    Update main_window.cpp

--- src/ForChecking/turtlebot3_manipulation_gui/src/main_window.cpp
+++ src/ForChecking/turtlebot3_manipulation_gui/src/main_window.cpp
@@ -1,22 +1,4 @@
-﻿/*******************************************************************************
-* Copyright 2020 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
-
-/* Authors: Ryan Shim */
-
-/*****************************************************************************
+﻿/*****************************************************************************
 ** Includes
 *****************************************************************************/
 
@@ -105,7 +87,7 @@ void MainWindow::on_btn_timer_start_clicked(void)
 void MainWindow::on_btn_init_pose_clicked(void)
 {
   std::vector<double> joint_angle;
-  double path_time = 2.0;
+  double path_time = 1.0;
   joint_angle.push_back(0.0);
   joint_angle.push_back(0.0);
   joint_angle.push_back(0.0);
@@ -123,7 +105,7 @@ void MainWindow::on_btn_init_pose_clicked(void)
 void MainWindow::on_btn_home_pose_clicked(void)
 {
   std::vector<double> joint_angle;
-  double path_time = 2.0;
+  double path_time = 1.0;
 
   joint_angle.push_back(0.0);
   joint_angle.push_back(-1.0);
