74cf3fbe923703e878a4dfbafc5cb60f52b524ab
MiGyeong <46590577+kmk3942@users.noreply.github.com>
2020-09-22 22:51:40 +0900

    Add files via upload

---
+++ src/barcode_cpp/barcode_image.cpp
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <zbar.h>
+#include <iostream>  
+#include <algorithm>
+#include <vector>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace zbar;
+
+typedef struct
+{
+	string type;
+	string data;
+	vector <Point> location;
+}decodedObject;
+
+// Find and decode barcodes and QR codes
+void decode(Mat &im, vector<decodedObject>&decodedObjects)
+{
+
+	// Create zbar scanner
+	ImageScanner scanner;
+
+	// Configure scanner
+	scanner.set_config(ZBAR_QRCODE, ZBAR_CFG_ENABLE, 1);
+
+	// Convert image to grayscale
+	Mat imGray;
+	cvtColor(im, imGray, CV_BGR2GRAY);
+
+	// Wrap image data in a zbar image
+	Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+
+	// Scan the image for barcodes and QRCodes
+	int n = scanner.scan(image);
+
+	// Print results
+	for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+	{
+		decodedObject obj;
+
+		obj.type = symbol->get_type_name();
+		obj.data = symbol->get_data();
+
+		// Print type and data
+		cout << "Type : " << obj.type << endl;
+		cout << "Data : " << obj.data << endl << endl;
+		decodedObjects.push_back(obj);
+	}
+}
+
+// Display barcode and QR code location 
+void display(Mat &im, vector<decodedObject>&decodedObjects)
+{
+	// Loop over all decoded objects
+	for (int i = 0; i < decodedObjects.size(); i++)
+	{
+		vector<Point> points = decodedObjects[i].location;
+		vector<Point> hull;
+
+		// If the points do not form a quad, find convex hull
+		if (points.size() > 4)
+			convexHull(points, hull);
+		else
+			hull = points;
+
+		// Number of points in the convex hull
+		int n = hull.size();
+
+		for (int j = 0; j < n; j++)
+		{
+			line(im, hull[j], hull[(j + 1) % n], Scalar(255, 0, 0), 3);
+		}
+
+	}
+
+	// Display results
+	imshow("Results", im);
+	waitKey(0);
+
+}
+
+int main()
+{
+
+	// Read image
+	Mat im = imread("barcode.png");
+
+	// Variable for decoded objects
+	vector<decodedObject> decodedObjects;
+
+	// Find and decode barcodes and QR codes
+	decode(im, decodedObjects);
+
+	// Display location
+	display(im, decodedObjects);
+
+	return 0;
+}
