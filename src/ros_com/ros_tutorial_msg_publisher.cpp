eddb3a7d654079a9194b3c15b3277042bc469613
MiGyeong <46590577+kmk3942@users.noreply.github.com>
2020-09-24 17:01:23 +0900

    Update ros_tutorial_msg_publisher.cpp

--- src/ros_com/ros_tutorial_msg_publisher.cpp
+++ src/ros_com/ros_tutorial_msg_publisher.cpp
@@ -1,28 +1,85 @@
 #include "ros/ros.h"
-#include "oroca_ros_tutorials/msgTutorial.h"
+#include "std_msgs/String.h"
 
+#include <sstream>
+
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "ros_tutorial_msg_publisher");
-	ros::NodeHandle nh;
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "talker");
 
-	ros::Publisher ros_tutorial_pub = nh.advertise<oroca_ros_tutorials::msgTutorial>("ros_tutorial_msg", 100);
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
 
-	ros::Rate loop_rate(10);
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
-	int count = 0;
-	while (ros::ok())	
-	{
-		oroca_ros_tutorials::msgTutorial msg;
+  ros::Rate loop_rate(10);
 
-		msg.data = count;
+  /**
+   * A count of how many messages we have sent. This is used to create
+   * a unique string for each message.
+   */
+  int count = 0;
+  while (ros::ok())
+  {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
 
-		ROS_INFO("send msg = %d", count);
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
 
-		ros_tutorial_pub.publish(msg);
+    ROS_INFO("%s", msg.data.c_str());
 
-		loop_rate.sleep();
-		++count;
-	}
-	return 0;
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
 }
