18443c443084478d427690c3f35819d336255200
MiGyeong <46590577+kmk3942@users.noreply.github.com>
2020-09-24 16:59:49 +0900

    Update ros_tutorial_msg_subscriber.cpp

--- src/ros_com/ros_tutorial_msg_subscriber.cpp
+++ src/ros_com/ros_tutorial_msg_subscriber.cpp
@@ -1,26 +1,28 @@
-#include "ros/ros.h"                                            // import rospy
+#include "ros/ros.h"
 #include "oroca_ros_tutorials/msgTutorial.h"
-#include <std_msgs/String.h>                                    // import std_msgs.msg import String
-
-// def callback(data)
-void msgCallback(const oroca_ros_tutorials::msgTutorial::ConstPtr& msg)
-{
-	ROS_INFO("recieve msg: %d", msg->data
-        // rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-}
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "ros_tutorial_msg_subscriber");
-        //rospy.init_node("listener",anoymous=True)
-
+	ros::init(argc, argv, "ros_tutorial_msg_publisher");
 	ros::NodeHandle nh;
 
-	ros::Subscriber ros_tutorial_sub = nh.subscribe("ros_tutorial_msg", 10, msgCallback);
-        //rospy.Subscriber(:publisher", String callback)
+	ros::Publisher ros_tutorial_pub = nh.advertise<oroca_ros_tutorials::msgTutorial>("ros_tutorial_msg", 100);
 
-	ros::spin();
-        //rospy.spin()
-	
+	ros::Rate loop_rate(10);
+
+	int count = 0;
+	while (ros::ok())	
+	{
+		oroca_ros_tutorials::msgTutorial msg;
+
+		msg.data = count;
+
+		ROS_INFO("send msg = %d", count);
+
+		ros_tutorial_pub.publish(msg);
+
+		loop_rate.sleep();
+		++count;
+	}
 	return 0;
 }
