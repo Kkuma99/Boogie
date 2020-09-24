51b7602275e75504812da8acb6f8367f9d24e1e8
MinJi <46590583+Kkuma99@users.noreply.github.com>
2020-09-24 16:10:39 +0900

    Update ros_tutorial_msg_subscriber.cpp

--- src/ros_com/ros_tutorial_msg_subscriber.cpp
+++ src/ros_com/ros_tutorial_msg_subscriber.cpp
@@ -1,19 +1,26 @@
-#include "ros/ros.h"
+#include "ros/ros.h"                                            // import rospy
 #include "oroca_ros_tutorials/msgTutorial.h"
+#include <std_msgs/String.h>                                    // import std_msgs.msg import String
 
+// def callback(data)
 void msgCallback(const oroca_ros_tutorials::msgTutorial::ConstPtr& msg)
 {
-	ROS_INFO("recieve msg: %d", msg->data);
+	ROS_INFO("recieve msg: %d", msg->data
+        // rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "ros_tutorial_msg_subscriber");
+        //rospy.init_node("listener",anoymous=True)
+
 	ros::NodeHandle nh;
 
 	ros::Subscriber ros_tutorial_sub = nh.subscribe("ros_tutorial_msg", 10, msgCallback);
+        //rospy.Subscriber(:publisher", String callback)
 
 	ros::spin();
+        //rospy.spin()
 	
 	return 0;
 }
