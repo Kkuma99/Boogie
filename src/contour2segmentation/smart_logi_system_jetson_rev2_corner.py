8263c3ff61ff466bf01d7dd13e2d42228781d931
WHEREVER <46590578+byj0410@users.noreply.github.com>
2021-03-15 22:56:46 +0900

    Update smart_logi_system_jetson_rev2_corner.py
    add memo

--- src/contour2segmentation/smart_logi_system_jetson_rev2_corner.py
+++ src/contour2segmentation/smart_logi_system_jetson_rev2_corner.py
@@ -34,12 +34,12 @@ def box_detection(img_color, result, box):
     # 노이즈 제거
     kernel = np.ones((5,5),np.uint8) # 커널 크기는 5*5
     # opening = cv2.morphologyEx(bin,cv2.MORPH_OPEN,kernel, iterations = 3) # 오프닝 연산으로 배경 노이즈 제거
-    opening = cv2.morphologyEx(bin,cv2.MORPH_CLOSE,kernel, iterations = 3) # 클로징 연산으로 객체 내부 노이즈 제거
+    opening = cv2.morphologyEx(bin,cv2.MORPH_CLOSE, kernel, iterations = 3) # 클로징 연산으로 객체 내부 노이즈 제거
     # cv2.imshow('opening', opening) # 모폴로지 연산 후 생성된 바이너리 이미지 확인
     # cv2.waitKey()
 
     # 확실한 배경 확보
-    sure_bg = cv2.dilate(opening,kernel,iterations=3) 
+    sure_bg = cv2.dilate(opening, kernel, iterations=3) 
 
     # 뼈대 이미지
     dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
@@ -53,7 +53,7 @@ def box_detection(img_color, result, box):
     # cv2.waitKey()    
 
     # 확실한 배경 - 확실한 전경 = 모르는 부분
-    unknown = cv2.subtract(sure_bg,sure_fg) 
+    unknown = cv2.subtract(sure_bg, sure_fg) 
 
     # 이미지 라벨링
     ret, markers = cv2.connectedComponents(sure_fg) # 확실한 전경 영역을 라벨링 *마커는 0(배경)부터 지정됨
@@ -120,7 +120,7 @@ def box_detection(img_color, result, box):
         # 결과 이미지에 컨투어 표시
         # cv2.drawContours(result, contours, min_index, (0, 255, 0), 2)
 
-        ''' 결과 반환 '''
+        ''' 결과 반환: 박스가 카메라와 정방향 '''
         # 컨투어를 둘러싸는 가장 작은 사각형 그리기
         cnt = contours[min_index]
         rect = cv2.minAreaRect(cnt)
@@ -130,7 +130,7 @@ def box_detection(img_color, result, box):
         return result, box # 윤곽선 그려진 전체 이미지, 꼭짓점 반환
 
     else:
-        ''' 결과 반환 '''
+        ''' 결과 반환: 박스가 카메라와 정방향 X '''
         box = ((pos[0], pos[1]), (pos[2], pos[3]), (pos[4], pos[5]), (pos[6], pos[7])) # 꼭짓점 지정
         box = np.int0(box) # 정수형으로 변경
         result = cv2.drawContours(result, [box], 0, (0, 0, 255), 2) # 찾아낸 꼭짓점을 따라 윤곽선 그려줌
@@ -143,8 +143,8 @@ def get_box_info(img_color, result, box, barcode_data, inputBox, NUM_BOX):
 
     ''' 바코드 면적 계산 '''
     retval, bin_barcode = cv2.threshold(gray_barcode, 0.7*gray_barcode.max(), 255, cv2.THRESH_BINARY) # 바이너리 이미지 생성
-    kernel = np.ones((5,5),np.uint8)
-    opening = cv2.morphologyEx(bin_barcode,cv2.MORPH_CLOSE,kernel, iterations = 3) # 바코드 라벨 컨투어 추출 위해 안쪽을 채워줌
+    kernel = np.ones((5,5), np.uint8)
+    opening = cv2.morphologyEx(bin_barcode, cv2.MORPH_CLOSE, kernel, iterations = 3) # 바코드 라벨 컨투어 추출 위해 안쪽을 채워줌
     # cv2.imshow('barcode', opening) # 전처리된 바이너리 이미지 확인
     # cv2.waitKey()
 
@@ -197,15 +197,15 @@ def get_box_info(img_color, result, box, barcode_data, inputBox, NUM_BOX):
             box_h = int(round((max_area/(640*480))*300, 0)) # 높이 *바코드 면적과 전체화면의 비율에 비례상수 곱하여 계산
             box_k = 20 # 무게
             # box_k = int(barcode_data[3:5]) # 무게
-            # print(box_h)
-            # if box_l <= 1 or box_w <= 1: continue  # 
+            # print(box_h, box_k)
 
             if not int(barcode_data[1:3]) in inputBox[ord(barcode_data[0]) - 65]:  # 중복되는 데이터가 없다면
                 inputBox[ord(barcode_data[0]) - 65][int(barcode_data[1:3])] = {'l': box_l, 'w': box_w, 'h': box_h, 'k': box_k} # 박스의 정보 담아주기
                 NUM_BOX[ord(barcode_data[0]) - 65] += 1 # 박스의 개수 하나 늘려주기
-                print('Info of box: ', box_w, box_l, box_h, box_k)  # 상자의 정보 출력
+                print('Info of box: %dcm %dcm %dcm %dkg' % (box_w, box_l, box_h, box_k))  # 상자의 정보 출력
+                # print('Info of box: ', box_w, box_l, box_h, box_k)  # 상자의 정보 출력
 
-    return barcode_data, result
+    return result, barcode_data
 
 
 # 트럭 내부 모습을 시각화하는 함수
@@ -245,56 +245,57 @@ def calculate_loading_order(NUM_LOCAL, NUM_BOX, TRUCK_L, TRUCK_W, TRUCK_H, input
         while True:
             if finish[i] == NUM_BOX[i]: break # 해당 지역 상자들의 적재가 끝나면 종료
 
-            # endOfL(현재 층에서 가장 작은 길이를 측정하기 위한 변수) 계산
-            endOfL = TRUCK_L
-            for j in range(TRUCK_W): # Y축 방향으로 검사
+            # END_L 계산
+            END_L = TRUCK_L # 현재 층에서 가장 작은 길이를 측정하기 위한 변수
+            for y in range(TRUCK_W): # Y축 방향으로 검사
                 count_L = 0
-                while truck[count_L][j][floor * BOX_H] != 0:  # 빈 공간이 나올때까지 반복
+                while truck[count_L][y][floor * BOX_H] != 0:  # 빈 공간이 나올때까지 반복
                     if count_L == TRUCK_L - 1:  # 트럭의 인덱스 끝까지 가면 탈출
+                        count_L += 1  # 탈출하기 전 실제 길이를 구하기 위해 1을 더함
                         break
-                    count_L += 1  # endOfL을 계산하기 위해 count_L을 증가시킴
-                if count_L == TRUCK_L - 1:  # count_L이 TRUCK_L-1이어서 위 while문을 탈출했다면
-                    count_L += 1  # 실제 길이를 구하기 위해 1을 더함
-                # endOfL은 길이의 최소값이므로 최소값을 구함
-                if count_L < endOfL:
-                    endOfL = count_L
+                    count_L += 1  # END_L을 계산하기 위해 count_L을 증가시킴
+                if count_L < END_L: END_L = count_L # 최솟값 갱신
 
             # 한 층에 각 지역에 할당된 길이만큼 적재되었거나, 트럭 길이 끝까지 적재된 경우 층수 증가
-            if endOfL >= TRUCK_L * (sum_num_box / sum(NUM_BOX)) or endOfL >= TRUCK_L:
+            if END_L >= TRUCK_L * (sum_num_box / sum(NUM_BOX)) or END_L >= TRUCK_L: 
                 floor += 1
-                if floor > TRUCK_H / BOX_H - 1:
-                    floor = 0
+                if floor > TRUCK_H / BOX_H - 1: floor = 0 # 최대 층수를 넘었으면 초기화
 
             ''' 상자를 적재할 위치 계산 '''
-            min_L = TRUCK_L  # 적재된 상자들이 차지한 가장 작은 길이(최소값을 찾기 위해 큰 값으로 초기화)
-            measureMode = 0  # 측정 모드 플래그: 비어있는 공간의 너비를 측정하고 있으면 1, 아니면 0
-            for j in range(TRUCK_L):  # X축 방향으로 검사
+            MIN_L = TRUCK_L  # 적재된 상자들이 차지한 가장 작은 길이
+            measureMode = 0  # 플래그: 비어있는 공간의 너비를 측정하고 있으면 1, 아니면 0
+
+            for x in range(TRUCK_L):  # X축 방향으로 검사
                 count_W = 0
-                for k in range(TRUCK_W):  # Y축 방향으로 검사
-                    if truck[j][k][floor * BOX_H] == 0:  # 0이면(비어있는 공간이면)
+                for y in range(TRUCK_W):  # Y축 방향으로 검사
+
+                    if truck[x][y][floor * BOX_H] == 0:  # 0이면 (비어있는 공간이면)
                         if measureMode == 0:  # 측정 모드가 아니었다면
-                            if j < min_L:  # 적재한 상자들이 차지하는 공간의 길이의 최소값이면
+                            if x < MIN_L:  # 적재한 상자들이 차지하는 공간의 길이의 최소값이면
                                 measureMode = 1  # 측정 모드로 전환
-                                min_L = j  # 최소값 갱신
-                                pos_X = j  # 상자를 적재할 x축 좌표 저장
-                                pos_Y = k  # 상자를 적재할 y축 좌표 저장
-                                pos_Z = floor * BOX_H
+                                MIN_L = x  # 최소값 갱신
+                                pos_X = x  # 상자를 적재할 x축 좌표 저장
+                                pos_Y = y  # 상자를 적재할 y축 좌표 저장
+                                pos_Z = floor * BOX_H # 상자를 적재할 z축 좌표 저장 # 이 부분 수정하면 높이에 따라 적재 가능?
                                 count_W = 1  # 빈 공간의 너비를 세기 시작함
-                        else:  # 측정모드이면
+                        else:  # 측정모드면
                             count_W += 1  # 상자가 들어갈 수 있는 너비를 측정하기 위해 count_W를 증가
-                    else:  # 1 또는 2이면(막혀있는 공간이면)
-                        if measureMode == 1:  # 측정 모드였다면
-                            measureMode = 0  # 측정 모드 해제
-                if count_W > 0:  # 해당 줄에 빈 공간이 있었다면
-                    break  # j에 대한 for 문 탈출
+
+                    else:  # 1 또는 2이면 (막혀있는 공간이면)
+                        if measureMode == 1: measureMode = 0  # 측정 모드였다면 측정 모드 해제
+
+                if count_W > 0: break # 해당 줄에 빈 공간이 있었다면 x에 대한 반복문 탈출
 
             ''' 적재할 상자 선택(5가지 조건 확인) '''
             max_box_W = 0  # count_W 너비 안에 들어갈 수 있는 최대 너비의 상자 너비
-            for j in range(NUM_BOX[i]):
+
+            for j in range(NUM_BOX[i]): # 박스 하나하나에 대해 모두 검사
                 cannot_load = 0
-                if check[i][j] == 0 and j in inputBox[i]:
-                    if inputBox[i][j]['w'] <= count_W:  # 1. 아직 적재하지 않은 상자이고, 너비가 count_W 이하면
+
+                if check[i][j] == 0 and j in inputBox[i]:    # 0. 아직 적재하지 않은 상자이고
+                    if inputBox[i][j]['w'] <= count_W:       # 1. 너비가 count_W 이하면
                         if inputBox[i][j]['w'] > max_box_W:  # 2. 최대 너비를 가진 상자를 찾음
+                            # 이 부분에 무게에 대한 조건 추가? 최대 무게를 가진 상자를 찾음
 
                             # 3. 해당 위치에 상자를 적재했을 때 트럭 높이를 넘지 않는지 확인
                             count_H = 0  
@@ -310,7 +311,7 @@ def calculate_loading_order(NUM_LOCAL, NUM_BOX, TRUCK_L, TRUCK_W, TRUCK_H, input
                             if pos_Z != 0:  # 가장 아래층인 경우는 제외
                                 for x in range(inputBox[i][j]['l']):
                                     for y in range(inputBox[i][j]['w']):
-                                        if truck[pos_X + x][pos_Y + y][pos_Z - 1] == 0: # 막혀있지 않다면
+                                        if truck[pos_X + x][pos_Y + y][pos_Z - 1] == 0: # 막혀있지 않다면 # 이 부분 수정하면 높이에 따라 적재 가능?2
                                             cannot_load = 1 # 적재할 수 없음
                                             break
                                 if cannot_load == 1: continue # 불합격
@@ -321,7 +322,7 @@ def calculate_loading_order(NUM_LOCAL, NUM_BOX, TRUCK_L, TRUCK_W, TRUCK_H, input
             ''' 상자 적재 또는 빈 공간 채우기 '''
             if max_box_W == 0:  # 해당 공간에 적재할 수 있는 상자가 없다면 빈공간 채우기
                 for y in range(count_W):
-                    for z in range(BOX_H):
+                    for z in range(BOX_H): # 이 부분 수정하면 높이에 따라 적재 가능?3
                         truck[pos_X][pos_Y + y][pos_Z + z] = 2 # 빈공간은 2로 채우기
 
             else:  # 해당 공간에 적재할 수 있는 상자가 있다면 상자 적재
@@ -385,8 +386,8 @@ LOCAL_B = 1
 LOCAL_C = 2
 
 # 박스 정보
-BOX_H = 5 # 상자 높이
-box = ()  # 박스의 윤곽을 그리기 위한 튜플
+BOX_H = 5     # 상자 높이
+box = ()      # 박스의 윤곽을 그리기 위한 튜플
 inputBox = {} # 박스의 정보를 담는 딕셔너리
 for i in range(0, NUM_LOCAL):
     inputBox[i] = {}
@@ -401,8 +402,8 @@ truck = np.zeros((TRUCK_L, TRUCK_W, TRUCK_H), dtype=np.int8)
 barcode_data = "XXX"
 
 # 이미지 읽어오기 (640*480)
-cap = cv2.VideoCapture('/dev/video1')  # 내장카메라: 0 / USB카메라: 1
-cap.set(cv2.CAP_PROP_FPS, 30)          # 프레임속도 30으로 설정
+cap = cv2.VideoCapture('/dev/video1')      # 내장카메라: 0 / USB카메라: 1
+cap.set(cv2.CAP_PROP_FPS, 30)              # 프레임속도 30으로 설정
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)     # 프레임 너비 640으로 설정
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)    # 프레임 높이 480으로 설정
 cv2.namedWindow('LOGI', cv2.WINDOW_NORMAL) # 화면 설정
@@ -412,11 +413,11 @@ while True:
     ''' 1. 이미지 읽어오기 '''
     ret, img_color = cap.read() # 카메라로부터 이미지를 읽어옴
     if not ret: continue        # 캡처에 실패할 경우 반복문의 첫 줄부터 수행하도록 함
-    result = img_color.copy()   # 화면에 표시하기 위해 원본 이미지를 결과 이미지에 복사
+    result = img_color.copy()   # 원본 이미지를 결과 이미지에 복사
 
     ''' 2. 상자 인식 및 정보 전송 '''
     result, box = box_detection(img_color, result, box)  # 상자 인식
-    barcode_data, result = get_box_info(img_color, result, box, barcode_data, inputBox, NUM_BOX)  # 상자 정보
+    result, barcode_data = get_box_info(img_color, result, box, barcode_data, inputBox, NUM_BOX)  # 상자 정보
     send_data_to_host(barcode_data)  # 바코드 데이터를 Host PC로 전송
     cv2.imshow('LOGI', result)  # 화면에 표시
 
